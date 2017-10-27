7153418c08511f1e80f95cc1cd89ad4672e99741
clouless <github@gruenewaldt.net>
2017-10-27 21:40:34 +0200

    export service

--- src/components/story.module.ts
+++ src/components/story.module.ts
@@ -12,7 +12,7 @@ import { CloukitStoryPrismCssWrapperComponent } from './children/prism-css-wrapp
 
 @NgModule({
   imports: [ CommonModule ],
-  exports: [ CloukitStoryComponent ],
+  exports: [ CloukitStoryComponent, CloukitStoryService ],
   declarations: [ CloukitStoryComponent, CustomSafeHtmlPipe, CloukitStoryPrismCssWrapperComponent ],
   providers: [ CloukitStoryService ],
   entryComponents: [ ],
--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CloukitStoryModule } from '../index';
 import { DemoComponent } from './demo.component';
 import { Story00Component } from './stories/story-00';
+import { CloukitStoryService } from '../components/story.service';
+import demoStoriesSource from './demo-stories-source.json';
 
 @NgModule({
   declarations: [ DemoComponent, Story00Component ],
@@ -14,4 +16,14 @@ import { Story00Component } from './stories/story-00';
   providers: [ ],
   bootstrap: [ ],
 })
-export class DemoModule {}
+export class DemoModule {
+  constructor(private cloukitStoryService: CloukitStoryService) {
+    //
+    // REGISTER STORY SOURCES
+    //
+    const keys = Object.keys(demoStoriesSource);
+    for (let i=0; i<keys.length; i++) {
+      this.cloukitStoryService.addSource(keys[i], demoStoriesSource[keys[i]]);
+    }
+  }
+}
